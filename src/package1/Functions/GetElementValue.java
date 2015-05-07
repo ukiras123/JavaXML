cb230348fdaf99ee10fb987e5345f3ffbc40c4ab
Kiran Gautam <ukiras@gmail.com>
2015-05-07 15:08:45 -0700

    Get the ElementValue

--- src/package1/Functions/GetElementValue.java
+++ src/package1/Functions/GetElementValue.java
@@ -1,38 +1,53 @@
 package package1.Functions;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+
 import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class GetElementValue {
 
-   public static void main(String argv[]) {
+	 public static void main(String[] args) throws IOException {
+		 
+		String xmlFilePath = "C:\\Users\\Kiran\\Desktop\\XML XSD Validation\\catalogResponse.xml";
 
-    }
-   
-    public void getEmelent(String URL, String element){
+		try {
+	
+			File xmlFile = new File(xmlFilePath);
 
-    	String URL1 = URL;
-    	String output = null;
-    	String output1 = null;
-    try {
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    Document doc = factory.newDocumentBuilder().parse(URL1); 
-     doc.getDocumentElement().normalize();      
-     NodeList nList = doc.getElementsByTagName(element);     
-     for (int temp = 0; temp < nList.getLength(); temp++) {
-     Node nNode = nList.item(temp);
-          if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-     Element eElement = (Element) nNode; 
-     output = eElement.getElementsByTagName(element).item(0).getTextContent();
-     output1 = output1  + output;
-        }
-     }
-    } catch (Exception e) {
-    	System.out.println(e);
-     //return e.getMessage();
-    }
-	System.out.println(output1); 
-}
+			DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+
+			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+			Document doc = documentBuilder.parse(xmlFile);
+
+			doc.getDocumentElement().normalize();
+
+			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
+			NodeList nodeList = doc.getElementsByTagName("catalog");
+
+			for (int itr = 0; itr < nodeList.getLength(); itr++) {
+
+				Node node = nodeList.item(itr);
+
+				System.out.println("\nNode Name :" + node.getNodeName());
+
+				if (node.getNodeType() == Node.ELEMENT_NODE) 
+				{
+					Element eElement = (Element) node;
+					NodeList nodeList2 = doc.getElementsByTagName("priceGroups");									
+					System.out.println(eElement.getElementsByTagName("priceGroups").item(0).getTextContent());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
